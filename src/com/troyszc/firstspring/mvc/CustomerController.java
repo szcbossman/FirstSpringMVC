f9845971b7e7bc96a94593f7b3084aaf57623d90
szcbossman <songzicheng3@gmail.com>
2018-07-21 16:04:30 -0400

    added String Trimmer for whitespace with bindingResult always right problem

--- src/com/troyszc/firstspring/mvc/CustomerController.java
+++ src/com/troyszc/firstspring/mvc/CustomerController.java
@@ -1,8 +1,11 @@
 package com.troyszc.firstspring.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,13 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
 
     @RequestMapping("/showForm")
     public String showForm(Model theModel) {
@@ -21,13 +31,13 @@ public class CustomerController {
 
     @RequestMapping("/processForm")
     public String processForm(
-            @Valid @ModelAttribute("customer") Customer theCustomer,
-            BindingResult theBindingResult) {
+            @Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+        System.out.println("DEBUG: " + theBindingResult.getModel());
         if (theBindingResult.hasErrors()) {
             System.out.println("in bindingResult error");
             return "customer-form";
         } else {
-            System.out.println("in bindingResult normal "+ theBindingResult);
+            System.out.println("DEBUG: in bindingResult normal "+ "lastName:|" + theCustomer.getLastName() + "|");
             return "customer-confirmation";
         }
     }
