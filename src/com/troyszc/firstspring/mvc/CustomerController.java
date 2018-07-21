e7973a5e2565ead5d5329dda99c3ba354c43de22
szcbossman <songzicheng3@gmail.com>
2018-07-21 14:27:38 -0400

    added CustomerController's validation

--- src/com/troyszc/firstspring/mvc/CustomerController.java
+++ src/com/troyszc/firstspring/mvc/CustomerController.java
@@ -2,7 +2,11 @@ package com.troyszc.firstspring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
@@ -12,5 +16,14 @@ public class CustomerController {
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
     }
 }
